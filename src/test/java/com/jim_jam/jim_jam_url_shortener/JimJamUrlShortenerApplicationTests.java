7c2cec21f6964282a0dd2c2f2aab690a8be786f6
Jimmy <tarunpreetsingh16@gmail.com>
2024-11-03 22:24:24 -0500

    init code

---
+++ src/test/java/com/jim_jam/jim_jam_url_shortener/JimJamUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jim_jam.jim_jam_url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JimJamUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
